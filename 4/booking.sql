9e2bbe42541c036187dfac18734f9104665ab614
Aevum Sonne <aevum.sonne@gmail.com>
2020-05-08 18:14:36 +0300

    Update 4.8, 4.9

--- 4/booking.sql
+++ 4/booking.sql
@@ -92,35 +92,64 @@
 -- клиентам в один номер. Записи в таблице room_in_booking с id_room_in_booking = 5 и 2154
 -- являются примером неправильного состояния, которые необходимо найти. Результирующий кортеж
 -- выборки должен содержать информацию о двух конфликтующих номерах.
-    SELECT *
+    SELECT booking1.id_booking, booking1.checkin_date, booking1.checkout_date, booking2.id_booking,
+           booking2.checkin_date, booking2.checkout_date
       FROM room_in_booking booking1, room_in_booking booking2
     WHERE booking1.id_room = booking2.id_room
         AND booking1.id_room_in_booking != booking2.id_room_in_booking
         AND (booking1.checkin_date BETWEEN booking2.checkin_date AND booking2.checkout_date
-            OR booking2.checkin_date BETWEEN booking1.checkin_date AND booking1.checkout_date)
-    GROUP BY booking1.id_room_in_booking, booking2.id_room_in_booking;
+            OR booking2.checkin_date BETWEEN booking1.checkin_date AND booking1.checkout_date);
 
 -- 8. Создать бронирование в транзакции.
     BEGIN TRANSACTION;
-        INSERT INTO booking
-        VALUES(77, '2019-07-11');
+      INSERT INTO client
+        (id_client, name, phone)
+      VALUES
+        ((SELECT id_client FROM client ORDER BY id_client DESC LIMIT 1) + 1,
+         'Jean-Paul Marat', '+49048383833');
+
+      INSERT INTO booking
+        (id_booking, id_client, booking_date)
+      VALUES
+        ((SELECT id_booking FROM booking ORDER BY id_booking DESC LIMIT 1) + 1,
+         (SELECT id_client FROM client ORDER BY id_client DESC LIMIT 1), '2019-04-04');
+
+      INSERT INTO room_in_booking
+        (id_room_in_booking, id_booking, id_room, checkin_date, checkout_date)
+      VALUES
+        ((SELECT id_room_in_booking FROM room_in_booking ORDER BY id_room_in_booking DESC LIMIT 1) + 1,
+         (SELECT id_booking FROM booking ORDER BY id_booking DESC LIMIT 1), 23, '2019-04-04', '2019-04-12');
     COMMIT;
 
+    SELECT *
+      FROM room_in_booking
+    ORDER BY id_room_in_booking DESC
+    LIMIT 1;
+
 -- 9. Добавить необходимые индексы для всех таблиц.
+    CREATE INDEX client_name_phone_index
+      ON client (name, phone);
+
+    CREATE INDEX hotel_name_stars_index
+      ON hotel (name, stars);
+
+    CREATE INDEX room_category_name_square_index
+      ON room_category (name, square);
+
     CREATE INDEX booking_id_client_index
-     ON booking (id_client);
+      ON booking (id_client);
 
-    CREATE INDEX room_in_booking_checkin_date_checkout_date_index
-     ON room_in_booking (checkin_date, checkout_date);
-
-    CREATE INDEX hotel_name_index
-     ON hotel (name);
+    CREATE INDEX booking_booking_date_index
+      ON booking (booking_date);
 
     CREATE INDEX room_id_hotel_id_room_category_index
-     ON room (id_hotel, id_room_category);
+      ON room (id_hotel, id_room_category);
 
-    CREATE INDEX room_category_name_id_room_category_index
-     ON room_category (name);
+    CREATE INDEX room_number_price_index
+      ON room (number, price);
 
-    CREATE INDEX room_in_booking_id_room_id_booking_index
-     ON room_in_booking (id_room, id_booking);
+    CREATE INDEX room_in_booking_id_booking_id_room_index
+      ON room_in_booking (id_booking, id_room);
+
+    CREATE INDEX room_in_booking_checkout_date_checkout_date_index
+      ON room_in_booking (checkout_date, checkout_date);
